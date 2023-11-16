449be009b338fc6620c33df6991454e0394dc9a8
L.C. Huirne <l.huirne@outlook.com>
2023-11-16 17:23:46 +0100

    improved

--- CheckPullThrough.py
+++ CheckPullThrough.py
@@ -1,6 +1,5 @@
 # This software component will check the given input design for Pull Through Failure.
 
-def check_pull_through(design_class):
+def check_pull_through(design_object):
     failure = True
     return failure
-print("hello wolrd")
