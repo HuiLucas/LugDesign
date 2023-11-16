f85ef40ca38bee06bd74a05c2af4c42d630d00bb
giulioruiz <151049093+giulioruiz@users.noreply.github.com>
2023-11-16 17:20:27 +0100

    yoyo

--- SelectFastenerConfiguration.py
+++ SelectFastenerConfiguration.py
@@ -1,6 +1,14 @@
 # This software component will select the fastener configuration according to WP4.4.
-# this checks if given w allows for the number and size of fastners given constraints
-def fastner_spacing_check(w,D_2,n_fast,material):
+# this checks if given w allows for the number and size of fasteners given constraints
+import numpy as np
+import random
+
+# this function creates a list that contains diameters that are going to be needed
+def assign_fastner_diameter_column(fastener_rows,fastener_diameter_upper_bound, fastner_diameter_lower_bound):
+    fastener_list = [random.uniform(fastner_diameter_lower_bound, fastener_diameter_upper_bound) for _ in range(fastener_rows)]
+    return fastener_list
+
+def fastener_spacing_check(w,D_2,n_fast,material):
     if material == "metal":
         if w > (n_fast-1)*2*D_2 + D_2 + 3*D_2:
             if w < (n_fast-1)*3*D_2 + D_2 + 3*D_2:
@@ -9,4 +17,6 @@ def fastner_spacing_check(w,D_2,n_fast,material):
         if w > (n_fast-1)*4*D_2 + D_2 + 3*D_2:
             if w < (n_fast-1)*5*D_2 + D_2 + 3*D_2:
                 return True
-print(fastner_spacing_check(0.2,0.01,4,"metal"))    
+
+
+while:
