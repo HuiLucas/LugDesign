a51af653aa2b937196161806a2070012ec201949
aitorbilbao <aitorbilbao25@gmail.com>
2023-11-16 16:32:03 +0100

    1

--- LocalLoadCalculatorAndLugDesignerAndLugConfigurator.py
+++ LocalLoadCalculatorAndLugDesignerAndLugConfigurator.py
@@ -1,3 +1,6 @@
 # This software component will calculate the local loads on a parametric model of the lug based on the given global
 # loads, and then choose values for the variables of the lug such that the lug can sustain these loads (see WP4.3).
 # This Software Component will also decide between a 1-lug or 2-lug configuration.
+
+N_Lugs=1
+N_flanges=2
