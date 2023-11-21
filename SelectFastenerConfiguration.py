dbf19a2e5e87afa477ac436e5f4ba2aec7f8d0fb
L.C. Huirne <l.huirne@outlook.com>
2023-11-21 13:32:04 +0100

    made example structure

--- SelectFastenerConfiguration.py
+++ SelectFastenerConfiguration.py
@@ -3,18 +3,28 @@
 import MainOptimizer
 import random as rnd
 
-def assign_diameter_list(fastener_rows,minimum_diameter,maximum_diameter):
-    diameter_list = [rnd.uniform(minimum_diameter, maximum_diameter) for _ in range(fastener_rows)]
+
+def assign_diameter_list(design_object):
+    diameter_list = [rnd.uniform(design_object.minimum_diameter, design_object.maximum_diameter) for _ in
+                     range(design_object.fastener_rows)]
     return diameter_list
+
 
 def fastener_spacing_check(design_object):
     if design_object.material == "metal":
-        if design_object.w > (design_object.n_fast-1)*2*design_object.D2 + design_object.D2 + 3*design_object.D2:
-            if design_object.w < (design_object.n_fast-1)*3*design_object.D2 + design_object.D2 + 3*design_object.D2:
+        if design_object.w > (
+                design_object.n_fast - 1) * 2 * design_object.D2 + design_object.D2 + 3 * design_object.D2:
+            if design_object.w < (
+                    design_object.n_fast - 1) * 3 * design_object.D2 + design_object.D2 + 3 * design_object.D2:
                 return True
     if design_object.material == "composite":
-        if design_object.w > (design_object.n_fast-1)*4*design_object.D2 + design_object.D2 + 3*design_object.D2:
-            if design_object.w < (design_object.n_fast-1)*5*design_object.D2 + design_object.D2 + 3*design_object.D2:
+        if design_object.w > (
+                design_object.n_fast - 1) * 4 * design_object.D2 + design_object.D2 + 3 * design_object.D2:
+            if design_object.w < (
+                    design_object.n_fast - 1) * 5 * design_object.D2 + design_object.D2 + 3 * design_object.D2:
                 return True
     return False
-print(fastener_spacing_check(MainOptimizer.initial_design))
+
+
+test_design = MainOptimizer.DesignInstance(5, 2, 2, 2, 2, 2, 5, "metal")
+print(fastener_spacing_check(test_design))
