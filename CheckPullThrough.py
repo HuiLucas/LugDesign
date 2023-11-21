c9bc74b6da1f7dc76597ce3c9632a33a094ebd87
L.C. Huirne <l.huirne@outlook.com>
2023-11-21 13:59:36 +0100

    made example structure

--- CheckPullThrough.py
+++ CheckPullThrough.py
@@ -1,5 +1,9 @@
 # This software component will check the given input design for Pull Through Failure.
+import MainOptimizer
 
+debug_design = MainOptimizer.DesignInstance(5, 2, 2, 2, 2, 2, 5, "metal")
 def check_pull_through(design_object):
     failure = True
     return failure
+
+print(check_pull_through(debug_design))
