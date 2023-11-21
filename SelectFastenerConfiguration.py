44de4467359e48ea2840da3f0ca0d9465925e7d5
L.C. Huirne <l.huirne@outlook.com>
2023-11-21 13:36:17 +0100

    made example structure

--- SelectFastenerConfiguration.py
+++ SelectFastenerConfiguration.py
@@ -4,6 +4,10 @@ import MainOptimizer
 import random as rnd
 import numpy as np
 
+debug_design = MainOptimizer.DesignInstance(5, 2, 2, 2, 2, 2, 5, "metal")
+debug_design.minimum_diameter = 3
+debug_design.maximum_diameter = 5
+
 
 def assign_diameter_list(design_object):
     diameter_list = [rnd.uniform(design_object.minimum_diameter, design_object.maximum_diameter) for _ in
@@ -11,22 +15,28 @@ def assign_diameter_list(design_object):
     return diameter_list
 
 
+print(assign_diameter_list(debug_design))
+
+
 # this checks if given w allows for the number and size of fasteners
-#for now only the case if the fastener diameter is constant
+# for now only the case if the fastener diameter is constant
 def fastener_spacing_check(design_object):
     if design_object.material == "metal":
-        if design_object.w > (design_object.n_fast-1)*2*design_object.D2 + design_object.D2 + 3*design_object.D2:
-            if design_object.w < (design_object.n_fast-1)*3*design_object.D2 + design_object.D2 + 3*design_object.D2:
+        if design_object.w > (
+                design_object.n_fast - 1) * 2 * design_object.D2 + design_object.D2 + 3 * design_object.D2:
+            if design_object.w < (
+                    design_object.n_fast - 1) * 3 * design_object.D2 + design_object.D2 + 3 * design_object.D2:
                 return True
     if design_object.material == "composite":
-        if design_object.w > (design_object.n_fast-1)*4*design_object.D2 + design_object.D2 + 3*design_object.D2:
-            if design_object.w < (design_object.n_fast-1)*5*design_object.D2 + design_object.D2 + 3*design_object.D2:
+        if design_object.w > (
+                design_object.n_fast - 1) * 4 * design_object.D2 + design_object.D2 + 3 * design_object.D2:
+            if design_object.w < (
+                    design_object.n_fast - 1) * 5 * design_object.D2 + design_object.D2 + 3 * design_object.D2:
                 return True
     return False
 
 
-test_design = MainOptimizer.DesignInstance(5, 2, 2, 2, 2, 2, 5, "metal")
-print(fastener_spacing_check(test_design))
+print(fastener_spacing_check(debug_design))
 
-#4.5 calculating the c.g of the fasteners the input of the function
-#is a list of the different diameters from top to bottom in the vertical direction
+# 4.5 calculating the c.g of the fasteners the input of the function
+# is a list of the different diameters from top to bottom in the vertical direction
