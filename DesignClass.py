ff36c3f00e3b55dc941ba39966f9bd03de3330fb
giulioruiz <151049093+giulioruiz@users.noreply.github.com>
2023-11-21 16:47:31 +0100

    yoyo

--- DesignClass.py
+++ DesignClass.py
@@ -1,10 +1,11 @@
 class DesignInstance:
-    def __init__(self, h, t1, t2, t3, D1, D2, w, material):
+    def __init__(self, h, t1, t2, t3, D1, D2, w,l, material):
         self.h = h
         self.t1 = t1
         self.t2 = t2
         self.t3 = t3
         self.D1 = D1
         self.D2 = D2
+        self.l = l #length of backplate
         self.w = w
         self.material = material
