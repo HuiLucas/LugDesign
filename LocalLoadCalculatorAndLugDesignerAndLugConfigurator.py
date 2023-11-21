4dd33565301b1191950a31642a029976a6835464
aitorbilbao <aitorbilbao25@gmail.com>
2023-11-21 15:14:19 +0100

    1

--- LocalLoadCalculatorAndLugDesignerAndLugConfigurator.py
+++ LocalLoadCalculatorAndLugDesignerAndLugConfigurator.py
@@ -19,7 +19,7 @@ Mz=0 #to be changed
 #-------------------------
 # Material List:
 # DF= die forging
-# P = plate
+# P = plate 1
 Material = ['2014-T6(DF)', '7075-T6', '4130 Steel', '8630 Steel', '2024-T4', 'AZ91C-T6', '356-T6 Aluminium','2024-T3','2014-T6(P)']
 F_yield = [414, 503, 435, 550, 324, 145, 165, 345]
 
