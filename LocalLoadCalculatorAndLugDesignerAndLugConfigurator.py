d334c37622d5b8ec062561748ceabe9c9e4d28f6
Angel6871 <91727236+Angel6871@users.noreply.github.com>
2023-11-23 14:12:58 +0100

    Final Optimization

--- LocalLoadCalculatorAndLugDesignerAndLugConfigurator.py
+++ LocalLoadCalculatorAndLugDesignerAndLugConfigurator.py
@@ -101,11 +101,14 @@ def choose_kby(t,D,e ):
 
 #Optimisation for each material and compare the options
 #intial guesses for '2014-T6(DF-L)':
-initial_guess = [0.01, 0.005, 0.009]
+initial_guess = [0.05, 0.01, 0.02]
 material = '2014-T6(DF-L)'
 #e=radius outer flange, t=thickness, D=diameter of the inner circle, material
 
-### ATTENTION: optimise the density and the yield strength
+K_t = calculate_kt(initial_guess[0],initial_guess[1],material,initial_guess[2])
+K_ty = choose_kby(initial_guess[2],initial_guess[1],initial_guess[0])
+
+### ATTENTION: optimise the mass and the yield strength
 def objective_function(variables, material = material):
     e, t, D = variables
     volume = calculate_vol(t,e,D)
@@ -119,7 +122,7 @@ def volume_constraint(variables):
     e, t, D = variables
     return calculate_vol(t,e,D)
 
-def principal_constraint(variables, material = material):
+def principal_constraint(variables):
     e, t, D = variables
     #K_t = calculate_kt(e,D,material,t)
     #K_ty = choose_kby(t,D,e)
@@ -129,7 +132,7 @@ def principal_constraint(variables, material = material):
         if i == material:
             Fy = F_yield[Material.index(i)]
             break
-    return (Fy/(calculate_kt(e,D,material,t) * Fy * A_t))**1.6 + (Fz/(choose_kby(t,D,e) * A_br * Fy))**1.6 - 1
+    return (Fy/(K_t * Fy * A_t))**1.6 + (Fz/(K_ty * A_br * Fy))**1.6 - 1
 
 constraints = [
     {'type': 'ineq', 'fun': volume_constraint},
@@ -140,8 +143,13 @@ constraints = [
 method = 'SLSQP'
 
 # Call the minimize function
-#result = minimize(objective_function, initial_guess, args=(material), method=method, constraints=constraints)
+result = minimize(objective_function, initial_guess, method=method, constraints=constraints, options={'disp': True}, tol=1e-6)
 
 # Print the result
-#print("Optimized variables:", result.x)
-#print("Minimum value of the objective function:", result.fun)
+if result.success:
+    print("Optimization converged successfully.")
+    print("Optimized variables:", result.x)
+    print("Minimum value of the objective function:", result.fun)
+else:
+    print("Optimization did not converge. Check the result message for more information.")
+    print("Message:", result.message)
