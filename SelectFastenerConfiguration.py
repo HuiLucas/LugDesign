2b31f63679c98245b83b24b55b3dbbf39abd8dfe
giulioruiz <151049093+giulioruiz@users.noreply.github.com>
2023-11-23 17:04:29 +0100

    yoyo

--- SelectFastenerConfiguration.py
+++ SelectFastenerConfiguration.py
@@ -8,8 +8,6 @@ import numpy as np
 
 debug_design = DesignClass.DesignInstance(2,2,2,2,2,6,"metal",10,6,10,10,[(3,3),(6,3),],[2,2])
 
-
-
 #check wether spacing constraints detailed in 4.4 are met, inputs are list of coordinates and list of diameter sizes
 def fastener_spacing_check(design_object):
     np_D2_list = np.array(design_object.D2_list)
@@ -22,7 +20,7 @@ def fastener_spacing_check(design_object):
     elif design_object.material == "composite":
         lower_limit = 4 * np.max(np_D2_list)
         upper_limit = 5 * np.max(np_D2_list)
-
+    #for loop checks wether the holes are within the margin of 2 * D2 from edges. For every hole
     for i in range(len(np_D2_list)):
         if not np_hole_coordinate_list[i,0] > 2 * np_D2_list[i] or design_object.length - np_hole_coordinate_list[i,0] > 2 * np_D2_list[i]:
             if np_hole_coordinate_list[i,0] - 2 * np_D2_list[i] <= 0:
@@ -34,7 +32,8 @@ def fastener_spacing_check(design_object):
                 return False , i , np_hole_coordinate_list[i,1] - 2 * np_D2_list[i] <=0
             elif design_object.length - np_hole_coordinate_list[i,1] - 2 * np_D2_list[i] <=0:
                 return False , i , np_hole_coordinate_list[i,1] - 2 * np_D2_list[i] <= 0
-
+    #lower limit and upper limit are calculated based on the material and the for loop checks wether the center to
+    #center constraint complies.
     for i in range(len(np_D2_list)):
         for k in range(i + 1, len(np_D2_list)):
             distance_x = np_hole_coordinate_list[i][0] - np_hole_coordinate_list[k][0]
