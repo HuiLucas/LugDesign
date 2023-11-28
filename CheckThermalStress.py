9cdcfac70a28a3f6c806b47f58315956cf466fd3
giulioruiz <151049093+giulioruiz@users.noreply.github.com>
2023-11-28 15:36:58 +0100

    yoyo

--- CheckThermalStress.py
+++ CheckThermalStress.py
@@ -1,1 +1,15 @@
 # This software component will check the given input design for Thermal Stress Failure.
+
+materials = [
+    {'material': '2014-T6(DF-L)', 'thermal_expansion_coefficient': 23},
+    {'material': '2014-T6(DF-LT)', 'thermal_expansion_coefficient': 23},
+    {'material': '2014-T6(P)', 'thermal_expansion_coefficient': 23},
+    {'material': '7075-T6(P)', 'thermal_expansion_coefficient': 23},
+    {'material': '7075-T6(DF-L)', 'thermal_expansion_coefficient': 23},
+    {'material': '7075-T6(DF-LT)', 'thermal_expansion_coefficient': 23},
+    {'material': '4130 Steel', 'thermal_expansion_coefficient': 11.7},
+    {'material': '8630 Steel', 'thermal_expansion_coefficient': 12.2},
+    {'material': '2024-T4', 'thermal_expansion_coefficient': 23.8},
+    {'material': '356-T6 Aluminium', 'thermal_expansion_coefficient': 23.8},
+    {'material': '2024-T3', 'thermal_expansion_coefficient': 23.8}
+]
