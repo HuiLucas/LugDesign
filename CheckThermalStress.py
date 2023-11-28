d5e677770afb1f0b77e6dcebabb23c5da716caa2
giulioruiz <151049093+giulioruiz@users.noreply.github.com>
2023-11-28 16:26:12 +0100

    yoyo

--- CheckThermalStress.py
+++ CheckThermalStress.py
@@ -7,15 +7,20 @@ materials = [
     {'material': '7075-T6(P)', 'thermal_expansion_coefficient': 23},
     {'material': '7075-T6(DF-L)', 'thermal_expansion_coefficient': 23},
     {'material': '7075-T6(DF-LT)', 'thermal_expansion_coefficient': 23},
-    {'material': '4130 Steel', 'thermal_expansion_coefficient': 11.1},
-    {'material': '8630 Steel', 'thermal_expansion_coefficient': 11.3},
-    {'material': '2024-T4', 'thermal_expansion_coefficient': 23.2},
+    {'material': '4130 Steel', 'thermal_expansion_coefficient': 11.7},
+    {'material': '8630 Steel', 'thermal_expansion_coefficient': 12.2},
+    {'material': '2024-T4', 'thermal_expansion_coefficient': 23.8},
     {'material': '356-T6 Aluminium', 'thermal_expansion_coefficient': 23.8},
-    {'material': '2024-T3', 'thermal_expansion_coefficient': 21.6}
+    {'material': '2024-T3', 'thermal_expansion_coefficient': 23.8}
 ]
 
-# 4130 steel https://industeel.arcelormittal.com/fichier/ds-mold-4130/
-#8630 steel https://dl.asminternational.org/handbooks/edited-volume/9/chapter/113443/Thermal-Properties-of-Carbon-and-Low-Alloy-Steels
-# 2024-T4 https://www.gabrian.com/2024-aluminum-properties/
-# 356-T6 Aluminium https://metalitec.zriha.com/eng/raw-materials/a356-t6
-# 2024-T3 2024-T3 https://www.gabrian.com/2024-aluminum-properties/
+# assumption is that for the temperature the coefficient remains linear . mention limitation
+# aluminum https://ntrs.nasa.gov/api/citations/19720023885/downloads/19720023885.pdf
+#
+
+def thermal_stress_calculation(design_object, lower_temp , ref_temp , phi_list , materials):
+    temp_diff = math.abs(ref_temp - lower_temp)
+    np_d2_list = np.array(design_object.D2_list)
+    np_phi_list = np.
+    thermal_force = np_d2_list ** 2 / 4 * temp_diff *
+
