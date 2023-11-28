fe4cca02630b1f460af5cab7812702b21b07e286
veronicafossa <v.fossa@tudelft.nl>
2023-11-28 16:25:35 +0100

    hello

--- CheckThermalStress.py
+++ CheckThermalStress.py
@@ -1,7 +1,7 @@
 # This software component will check the given input design for Thermal Stress Failure.
 
 materials = [
-    {'material': '2014-T6(DF-L)', 'thermal_expansion_coefficient': 23},
+    {'material': '2014-T6(DF-L)', 'thermal_expansion_coefficient': 23 ,'elastic module': },
     {'material': '2014-T6(DF-LT)', 'thermal_expansion_coefficient': 23},
     {'material': '2014-T6(P)', 'thermal_expansion_coefficient': 23},
     {'material': '7075-T6(P)', 'thermal_expansion_coefficient': 23},
@@ -18,4 +18,5 @@ materials = [
 #8630 steel https://dl.asminternational.org/handbooks/edited-volume/9/chapter/113443/Thermal-Properties-of-Carbon-and-Low-Alloy-Steels
 # 2024-T4 https://www.gabrian.com/2024-aluminum-properties/
 # 356-T6 Aluminium https://metalitec.zriha.com/eng/raw-materials/a356-t6
-# 2024-T3 2024-T3 https://www.gabrian.com/2024-aluminum-properties/
+# 2024-T3 2024-T3 https://www.gabrian.com/2024-aluminum-properties
+
