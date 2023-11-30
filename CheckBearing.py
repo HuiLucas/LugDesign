fixed units for bearing check

CheckBearing.py
@@ -94,7 +94,7 @@ print(get_in_plane_loads(debug_design_2, debug_loads))
 print(calculate_centroid(debug_design_2))
 print(get_F_in_plane_My(debug_design_2, debug_loads))
 
-def check_bearing_stress(design_object, load_object2):
+def check_bearing_stress(design_object, load_object2,thermal_force):
 
     M_y = get_in_plane_loads(design_object, load_object2)[2]
     S = np.sum(((np.array(design_object.hole_coordinate_list)[:, 0] - centroid_x) ** 2 + (np.array(design_object.hole_coordinate_list)[:, 1] - centroid_z) ** 2) * np.pi * np.array(design_object.D2_list) ** 2 / 4) / (1000 ** 4)
@@ -105,13 +105,13 @@ def check_bearing_stress(design_object, load_object2):
         distance_z_1 = design_object.hole_coordinate_list[i][1] - centroid_z
         r = np.sqrt(distance_x_1 ** 2 + distance_z_1 ** 2)/1000
         A = (np.pi * design_object.D2_list[i] ** 2 / 4)/(1000**2)
-        P = np.sqrt(get_in_plane_loads(design_object, load_object2)[0]**2 + get_in_plane_loads(design_object, load_object2)[1]**2 + (M_y * A * r / S)**2)
+        P = np.sqrt(get_in_plane_loads(design_object, load_object2)[0]**2 + get_in_plane_loads(design_object, load_object2)[1]**2 + (M_y * A * r / S)**2 + thermal_force**2)
         sigma = (P / (design_object.D2_list[i] * design_object.t2))
         sigma0.append(sigma)
         print(sigma)
     if np.max(sigma0) < design_object.yieldstrength:
         print("Bearing Stress Check Pass")
 
-check_bearing_stress(debug_design_2, debug_loads)
+check_bearing_stress(debug_design_2, debug_loads,1600)
 
 